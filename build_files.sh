94fb58abfca46c37bb925b73e268346868eef13c
Owoeye Emmanuel Ayomide <owoeyeemmanuel206@gmail.com>
2026-08-20 18:01:01 +0100

    fix(vercel): changed vercel confg

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
+set -e
 echo "BUILD START" 
-python3.12 -m pip install -r requirements.txt 
+python3.12 -m pip install --break-system-packages -r requirements.txt 
 python3.12 manage.py collectstatic --noinput --clear 
 echo "BUILD END"
